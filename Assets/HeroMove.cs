159551dcb55286ef8d45515665df823ef3b204ab
Andreas Wilcox <unknownbum@gmail.com>
2015-12-14 17:07:21 +0100

    Added Camera other stand assets

---
+++ Assets/HeroMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class HeroMove : MonoBehaviour
+{
+	void Update ()
+	{
+	    var multiplier = 2;
+	    if (Input.GetKeyDown(KeyCode.UpArrow))
+	    {
+	        transform.Translate(Vector3.forward*multiplier);
+	    }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * multiplier);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * multiplier);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * multiplier);
+        }
+	}
+}
