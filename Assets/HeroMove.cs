a0d913a0e7a18939f41a9942a21268b108a75891
Andreas Wilcox <unknownbum@gmail.com>
2015-12-14 18:27:47 +0100

    Added collision-handling for walking on tiles

--- Assets/HeroMove.cs
+++ Assets/HeroMove.cs
@@ -4,22 +4,30 @@ public class HeroMove : MonoBehaviour
 {
 	void Update ()
 	{
-	    var multiplier = 2;
-	    if (Input.GetKeyDown(KeyCode.UpArrow))
+        const int multiplier = 2;
+        var currentPos = new TilePos(transform.position/multiplier);
+	    var moveDirection = new TilePos(0, 0);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
 	    {
-	        transform.Translate(Vector3.forward*multiplier);
+	        moveDirection = new TilePos(0, 1);
 	    }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            transform.Translate(Vector3.back * multiplier);
+	        moveDirection = new TilePos(0, -1);
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.left * multiplier);
+            moveDirection = new TilePos(-1, 0);
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.right * multiplier);
+            moveDirection = new TilePos(1, 0);
         }
+
+	    if (RoomInfo.CanWalk(moveDirection + currentPos))
+	    {
+	        transform.Translate(moveDirection.ToV3()*multiplier);
+	    }
 	}
 }
