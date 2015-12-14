1113109ed6bf8155911fff5540e9542092c90209
Andreas Wilcox <unknownbum@gmail.com>
2015-12-14 18:56:10 +0100

    Can now generate multiple random tiles

--- Assets/GenerateTiles.cs
+++ Assets/GenerateTiles.cs
@@ -7,24 +7,27 @@ public class GenerateTiles : MonoBehaviour
     public int Columns;
     public int Rows;
 
-    public GameObject Tile;
-    public GameObject TileBottom;
+    public List<GameObject> Tiles;
     public float SideLength;
 
     [ContextMenu("Generate")]
     public void Generate()
     {
+        if (!Tiles.Any())
+        {
+            Debug.LogError("No tiles set for generator");
+            return;
+        }
+
         var tilesParent = GetTilesParent();
 
         for (var z = 0; z < Rows; z++)
         {
             for (var x = 0; x < Columns; x++)
             {
-                var tile = (GameObject)Instantiate(Tile, new Vector3(x*SideLength, 0, z*SideLength), Quaternion.AngleAxis(Random.Range(0, 4)*90, Vector3.up));
+                var tileTemplate = Tiles[Random.Range(0, Tiles.Count)];
+                var tile = (GameObject)Instantiate(tileTemplate, new Vector3(x*SideLength, 0, z*SideLength), Quaternion.AngleAxis(Random.Range(0, 4)*90, Vector3.up));
                 tile.transform.SetParent(tilesParent.transform, true);
-
-                var tileBottom = Instantiate(TileBottom);
-                tileBottom.transform.SetParent(tile.transform, false);
             }
         }
     }
