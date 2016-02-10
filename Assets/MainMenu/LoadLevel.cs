9ebd076109eb69c19564a63992be797d88ad12c9
Andreas Wilcox <UnknownBum@gmail.com>
2016-02-10 11:11:56 +0100

    Made basic main menu

---
+++ Assets/MainMenu/LoadLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    public void LoadPlay()
+    {
+        SceneManager.LoadScene("Play");
+    }
+
+    public void LoadEditor()
+    {
+        SceneManager.LoadScene("Editor");
+    }
+}
