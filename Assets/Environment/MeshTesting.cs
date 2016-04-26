9512c2de3b1754b13af49728377bcf63b9f3ad46
Andreas Wilcox <unknownbum@gmail.com>
2016-04-26 13:40:09 +0200

    Split up single level piece into multiple, added camera spin code on touch

---
+++ Assets/Environment/MeshTesting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshTesting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    GetComponent<MeshFilter>().sharedMesh = new Mesh()
+	        {
+	            vertices = new[] { new Vector3(0, 0), new Vector3(10, 0), new Vector3(10, 10), new Vector3(0, 10)  },
+                triangles = new [] { 0, 1, 2, 0, 2, 3 }
+	        };
+	}
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
